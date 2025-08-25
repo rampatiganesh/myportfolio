c5b1bc51fa02b6b9606bdd1431f96040b4e20c86
Vijaya R <102131218+rampatiganesh@users.noreply.github.com>
2025-08-25 15:54:37 -0500

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/myportfolio/",   // 👈 repo name must match
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
